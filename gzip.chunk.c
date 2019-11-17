8e54b11725c96abc8165b39947e6365c6096fe13
Vasilii Rogin <vasya@rogin.ru>
2019-11-17 21:30:21 +0200

    Trying to separate gzip chunks

--- gzip.chunk.c
+++ gzip.chunk.c
@@ -4,6 +4,24 @@
 char *compress_chunk(char *buf, int32_t buf_len, int32_t *outlen, char is_last)
 {
     printf("Compressing chunk len=%i\n", buf_len);
+
+    /*
+    int32_t gzip_blocks = buf_len / 0xffff + (buf_len % 0xffff == 0 ? 0 : 1);
+
+    if (buf_len == 0)
+    {
+        *outlen = 0;
+        return NULL;
+    };
+
+    *outlen = gzip_blocks * 5 + buf_len;
+    char *out = (char *)malloc(*outlen);
+
+    for (int32_t i = 0; i < gzip_blocks; i++)
+    {
+    }
+*/
+
     *outlen = buf_len + 5;
     char *out = (char *)malloc(*outlen);
     out[0] = is_last ? 1 : 0;
