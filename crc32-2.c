a4543dfafeb2dea17466177ae87d7378980ce7f4
Vasilii Rogin <vasilii@giosg.com>
2019-11-23 21:18:14 +0200

    Steps with crc

--- crc32-2.c
+++ crc32-2.c
@@ -27,9 +27,38 @@ uint32_t _crc32_for_byte(uint8_t byte)
 
 void init_table()
 {
-       for (uint32_t i = 0; i < 0x100; ++i)
+       for (uint32_t i = 0; i < 0x100; i++)
        {
               table[i] = _crc32_for_byte((uint8_t)i);
+       }
+}
+
+void crc32_no_padding(const uint8_t *data, uint32_t n_bytes, uint32_t *crc)
+{
+       for (uint32_t i = 0; i < n_bytes; i++)
+       {
+              uint8_t shifted_byte = (uint8_t)(*crc >> (8 * 3));
+              uint32_t xoring = table[shifted_byte];
+
+              uint8_t next_byte = data[i];
+
+              printf("i=%i shifted_byte=%02x xoring=%02x next_byte=%02x curCrc=%08x shiftedCrc=%08x nextCrc=%08x\n",
+                     i, shifted_byte, xoring, next_byte,
+                     *crc,
+                     (*crc << 8) | next_byte,
+                     ((*crc << 8) | next_byte) ^ xoring);
+
+              *crc = (*crc << 8) | next_byte;
+
+              *crc = *crc ^ xoring;
+       }
+}
+
+void _clean(uint8_t *data, uint32_t len)
+{
+       for (uint32_t i = 0; i < len; ++i)
+       {
+              data[i] = 0;
        }
 }
 
@@ -60,6 +89,12 @@ int main()
               return 5;
        };
 
+       if (_crc32_for_byte(0x40) != (uint32_t)((poly << 7)))
+       {
+              printf("err 6 %08x %08x", _crc32_for_byte(0x40), (uint32_t)((poly << 7)));
+              return 6;
+       };
+
        /*
        for (uint8_t i = 0; i < 10; ++i)
        {
@@ -73,5 +108,49 @@ int main()
        {
               return 1;
        };
-       printf("Done");
+       printf("Table done\n");
+
+       uint32_t crc = 0;
+       uint8_t data[8] = {0};
+       _clean(data, 8);
+       crc32_no_padding(data, 8, &crc);
+       if (crc != 0)
+       {
+              printf("Err %08x\n", crc);
+              return 1;
+       }
+
+       /*
+       printf("\nKek\n");
+       //crc = 0xFFFFFFFF;
+       _clean(data, 8);
+       // 6b656b0a
+       data[0] = 0x6b;
+       data[1] = 0x65;
+       data[2] = 0x6b;
+       data[3] = 0x0a;
+       crc32_no_padding(data, 8, &crc);
+       //crc = crc ^ 0xFFFFFFFF;
+
+       printf("0x6b656b0a crc = %08x\n", crc);
+       */
+
+       printf("\nKek\n");
+       crc = 0;
+       _clean(data, 8);
+       // 6b
+       // ???
+       // 0x39 - works
+       // 0x40 - not works
+       //    (0x40 = 0b01000000) = 64
+       //   poly = 1 0000 0100 1100 0001 0001 1101 1011 0111
+       data[0] = 0x40;
+
+       crc32_no_padding(data, 5, &crc);
+
+       printf("Lol %08x\n", crc);
+
+       printf("Done\n");
+
+       // http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
 }
