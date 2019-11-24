7199ca6e4c8c00589ff7942e81df51de6d2d6b3e
Vasilii Rogin <vasilii@giosg.com>
2019-11-24 14:46:12 +0200

    Steps

--- gzip.chunk.c
+++ gzip.chunk.c
@@ -3,25 +3,25 @@
 
 char *store_chunk(char *buf, int32_t buf_len, int32_t *outlen, char is_last)
 {
-    printf("\n\nGzip store chunk input buf_len=%i\n", buf_len);
+    // printf("\n\nGzip store chunk input buf_len=%i\n", buf_len);
     int32_t total_gzip_blocks = buf_len / 0xffff + (buf_len % 0xffff == 0 ? 0 : 1);
     if (buf_len == 0)
     {
         *outlen = 0;
         return NULL;
     };
-    printf("Gzip blocks = %i\n", total_gzip_blocks);
+    //  printf("Gzip blocks = %i\n", total_gzip_blocks);
 
     *outlen = total_gzip_blocks * 5 + buf_len;
     char *out = (char *)malloc(*outlen);
-    printf("outlen = %i\n", *outlen);
+    //  printf("outlen = %i\n", *outlen);
 
     for (int16_t current_gzip_block = 0; current_gzip_block < total_gzip_blocks; current_gzip_block++)
     {
 
         uint16_t len = current_gzip_block == total_gzip_blocks - 1 ? buf_len - current_gzip_block * 0xFFFF
                                                                    : 0xFFFF;
-        printf("Curent current_gzip_block is %i, len =%i\n", current_gzip_block, len);
+        // printf("Curent current_gzip_block is %i, len =%i\n", current_gzip_block, len);
 
         int32_t current_out_offset = current_gzip_block * (0xFFFF + 5);
 
